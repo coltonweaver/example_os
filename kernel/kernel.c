1bf2c8c5e2a8834cbde6ac2ffb20f55a3a2bb88f
Colton Weaver <cbw1996@tamu.edu>
2018-09-26 16:03:46 -0500

    Added IRQ handlers and a timer to test it.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,11 +1,13 @@
-#include "../drivers/screen.h"
-#include "util.h"
 #include "../cpu/isr.h"
-#include "../cpu/idt.h"
+#include "../cpu/timer.h"
+#include "../drivers/screen.h"
 
 void main() {
     clear_screen();
     isr_install();
+
+    asm volatile("sti");
+    init_timer(50);
 
     /* Test interrupts */
     __asm__ __volatile__("int $2");
--- kernel/util.c
+++ kernel/util.c
@@ -17,4 +17,23 @@ void int_to_ascii(int n, char str[]) {
 
     if (sign < 0) str[i++] = '-';
     str[i] = '\0';
+
+    reverse(str);
+}
+
+/* K&R */
+void reverse(char s[]) {
+    int c, i, j;
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+/* K&R */
+int strlen(char s[]) {
+    int i = 0;
+    while (s[i] != '\0') ++i;
+    return i;
 }
