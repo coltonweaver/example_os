b14d610e596ee8acb3b3377fb99750699546b7fb
Colton Weaver <cbw1996@tamu.edu>
2018-09-26 12:27:39 -0500

    Lesson 15 finished. Now can interact with ports.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,31 @@
-void dummy_test_entrypoint() {
-}
+#include "../drivers/ports.h"
 
 void main() {
-    char * video_memory = (char *) 0xb8000;
-    *video_memory = 'X';
+    /* Screen cursor position: ask VGA control register (0x3d4) for bytes
+     * 14 = high byte of cursor and 15 = low byte of cursor. */
+    port_byte_out(0x3d4, 14);
+    /* Data is returned in VGA data register (0x3d5) */
+    int position = port_byte_in(0x3d5);
+    position = position << 8; /* This was the high byte */
+
+    /* Now retrieve the low byte */
+    port_byte_out(0x3d4, 15);
+    position += port_byte_in(0x3d5);
+
+    /* VGA 'cells' consist of the character and its control data
+     * e.g. 'white on black background', 'red text on white bg', etc */
+    int offset_from_vga = position * 2;
+
+    /* Now you can examine both variables using gdb, since we still
+     * don't know how to print strings on screen. Run 'make debug' and
+     * on the gdb console:
+     * breakpoint kernel.c:21
+     * continue
+     * print position
+     * print offset_from_vga
+    */
+
+    char * vga = 0xb8000;
+    vga[offset_from_vga] = 'X';
+    vga[offset_from_vga+1] = 0x0f; 
 }
