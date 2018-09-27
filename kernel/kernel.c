7b71783fa22ff87ee46fb79669875813ff375b7d
Colton Weaver <cbw1996@tamu.edu>
2018-09-27 10:46:26 -0500

    Keyboard support added.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,15 +1,14 @@
 #include "../cpu/isr.h"
 #include "../cpu/timer.h"
 #include "../drivers/screen.h"
+#include "../drivers/keyboard.h"
 
 void main() {
     clear_screen();
     isr_install();
 
     asm volatile("sti");
-    init_timer(50);
+    // init_timer(50);
 
-    /* Test interrupts */
-    __asm__ __volatile__("int $2");
-    __asm__ __volatile__("int $3");
+    init_keyboard();
 }
